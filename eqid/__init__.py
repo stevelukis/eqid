32c79c6144a174cb9a86a0a21522c086340ab658
Steve Lukis <stevelukis88@gmail.com>
2021-09-16 19:39:37 +0800

    Obtain real time data from BMKG website

--- eqid/__init__.py
+++ eqid/__init__.py
@@ -1,13 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+_url = 'https://www.bmkg.go.id/'
+
+
 def _extract_data():
+    response = requests.get(_url)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    result = soup.find('span', attrs={'class': 'waktu'})
+    result = result.text.split(', ')
+    date = result[0]
+    time = result[1]
+
+    result = soup.find('div', attrs={'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+    result = result.findChildren('li')
+
+    magnitude = result[1].text
+    depth = result[2].text
+
+    location_raw = result[3].text
+    location_raw = location_raw.split(' LS - ')
+    lat = location_raw[0]
+    long = location_raw[1].replace(' BT', '')
+
+    center_desc = result[4].text
+    affected_desc = result[5].text
+
     data = dict()
-    data['date'] = '15 September 2021'
-    data['time'] = '07:49:47 WIB'
-    data['magnitude'] = 5.4
+    data['date'] = date
+    data['time'] = time
+    data['magnitude'] = magnitude
     data['depth'] = 20
-    data['location'] = {'lat': 1.28, 'long': 122.16}
-    data['center_description'] = 'Pusat gempa berada di laut 85 km BaratLaut Boalemo'
-    data['affected_description'] = \
-        'Dirasakan (Skala MMI): III Boalemo, II - III Buol, II Manado, II Bone Bolango, II Gorontalo, II Toli-toli'
+    data['location'] = {'lat': lat, 'long': long}
+    data['center_desc'] = center_desc
+    data['affected_desc'] = affected_desc
 
     return data
 
@@ -22,5 +50,5 @@ def console_print(result):
     print(f"Time: {result['time']}")
     print(f"Magnitude: {result['magnitude']}")
     print(f"Location: Latitude={result['location']['lat']} longitude={result['location']['long']}")
-    print(f"Center: {result['center_description']}")
-    print(f"Affected: {result['affected_description']}")
+    print(f"Center: {result['center_desc']}")
+    print(f"Affected: {result['affected_desc']}")
