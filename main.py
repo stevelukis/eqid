316353857656d70e2b6122f9cf532d6a42c7cea7
Steve Lukis <stevelukis88@gmail.com>
2021-09-16 10:28:16 +0800

    Move the basic functions to its own package

--- main.py
+++ main.py
@@ -1,31 +1,7 @@
 """
 Latest Earthquake Detection Package
 """
-
-
-def data_extranction():
-    data = dict()
-    data['date'] = '15 September 2021'
-    data['time'] = '07:49:47 WIB'
-    data['magnitude'] = 5.4
-    data['depth'] = 20
-    data['location'] = {'lat': 1.28, 'long': 122.16}
-    data['center_description'] = 'Pusat gempa berada di laut 85 km BaratLaut Boalemo'
-    data['affected_description'] = \
-        'Dirasakan (Skala MMI): III Boalemo, II - III Buol, II Manado, II Bone Bolango, II Gorontalo, II Toli-toli'
-
-    return data
-
-
-def console_print(result):
-    print('Latest Earthquake in Indonesia')
-    print(f"Date: {result['date']}")
-    print(f"Time: {result['time']}")
-    print(f"Magnitude: {result['magnitude']}")
-    print(f"Location: Latitude={result['location']['lat']} longitude={result['location']['long']}")
-    print(f"Center: {result['center_description']}")
-    print(f"Affected: {result['affected_description']}")
-
+from eqid import data_extranction, console_print
 
 if __name__ == '__main__':
     result = data_extranction()
