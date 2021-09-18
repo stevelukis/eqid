aa413c7db0867c79f2ab4ecfebbe79f521b5b166
Steve Lukis <stevelukis88@gmail.com>
2021-09-18 14:40:39 +0800

    Move example file to its own directory

---
+++ examples/main.py
@@ -0,0 +1,7 @@
+"""
+Latest Earthquake Detection Package
+"""
+import eqid
+
+result = eqid.get_data()
+eqid.console_print(result)
--- main.py
+++
@@ -1,8 +0,0 @@
-"""
-Latest Earthquake Detection Package
-"""
-import eqid
-
-if __name__ == '__main__':
-    result = eqid.get_data()
-    eqid.console_print(result)
